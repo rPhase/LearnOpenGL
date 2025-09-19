131a33250f6d27e4d654f3d37640d7e92f1c2984
rPhase <github@rphase.net>
2025-09-19 00:32:53 -0700

    reset back to previous step before exercises

--- 2 - Triangle/Main.cpp
+++ 2 - Triangle/Main.cpp
@@ -17,9 +17,7 @@ int main()
 
     // Create a GLFWwindow object of 800x600 pixels with a name
     GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-    // Exercise 2 - change size of the window to w=400, h=225
-    // Exercise 3 - Change name of window to "I made this!"
-    // GLFWwindow *window = glfwCreateWindow(400, 225, "I made this!", NULL, NULL);
+
     // Check if window fails to create
     if (window == NULL)
     {
@@ -35,35 +33,17 @@ int main()
     // Specify the viewport of OpenGl in the Window
     // x = 0, y = 0 to x = 800, y = 600
     glViewport(0, 0, 800, 600);
-    // glViewport(0, 0, 400, 225);
 
     // Specify the color of the background
-    // glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-    // Exercise 1 - Change color to orange
-    // glClearColor(1.0f, 0.55f, 0.0f, 1.0f);
+    glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
     // Clean the back buffer and assign a new color to it
-    // glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
     // Swap the back buffer with the front buffer
-    // glfwSwapBuffers(window);
-
-    // Hard Exercise 1
-    // Flucuate between variety of colors
-    float prev_time = float(glfwGetTime()); // Keep track of previous time
-    float angle = 0.0f;                     // control the color
+    glfwSwapBuffers(window);
 
     // Main loop
     while (!glfwWindowShouldClose(window))
     {
-        float time = float(glfwGetTime());
-        if (time - prev_time >= 0.05f) // every 0.1 seconds it's true
-        {
-            angle += 0.1f;    // change angle to change color
-            prev_time = time; // update previous time
-        }
-        // Use trig to change colors
-        glClearColor(float(sin(angle)), float(cos(angle)), float(tan(angle)), 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(window);
         // Take care of all GLFW events
         glfwPollEvents();
     }
